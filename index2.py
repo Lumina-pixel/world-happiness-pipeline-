0636cf7bd371247cd16400a3710e402a16561b03
Lumina-pixel <vaibhav.madhwal@gmail.com>
2026-07-10 00:42:49 +0530

    Clean up: remove all practice files

--- index2.py
+++
@@ -1,9 +0,0 @@
-sentence = input("enter a string: ")
-
-for ch in sentence.lower():
-    if ch in "aeiou":
-        print("string has a vowel")
-        break
-else:
-    print("no vowels")
-
--- main.py
+++
@@ -1,2 +0,0 @@
-print("hie")
-print("helololooolololo")
